ab43eda780c0eb358f16d80b7e0cbfcfb838ac38
shashankkatte <shashankanand.katte@gmail.com>
2021-05-04 12:42:05 -0400

    src : Add a sample webservices app

---
+++ 04_webservice_demo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+import "log"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+		w.Write([]byte("Hello, " + name))
+	})
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
